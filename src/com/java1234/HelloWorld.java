067740664facc1a68e45a703e6a8c9ccffdacd0b
java1234 <java1234@qq.com>
2021-08-01 12:29:08 +0800

    支付开发完成

--- src/com/java1234/HelloWorld.java
+++ src/com/java1234/HelloWorld.java
@@ -2,7 +2,8 @@ package com.java1234;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
+		System.out.println("Hello World");
+		System.out.println("Pay÷ß∏∂");
 	}
 	
 }
